228153e6b01abae9aec90e938d5e48d8d5c42ec5
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-07 17:06:21 -0300

    scroll bar card content

--- src/app/components/card/card-content.component.ts
+++ src/app/components/card/card-content.component.ts
@@ -17,6 +17,25 @@ import { Component, Input } from '@angular/core'
         height: 200px;
         overflow: auto;
       }
+
+      div::-webkit-scrollbar,
+      main::-webkit-scrollbar,
+      ol::-webkit-scrollbar,
+      pre::-webkit-scrollbar,
+      span::-webkit-scrollbar,
+      ul::-webkit-scrollbar {
+        width: 0.4rem;
+        height: 0.4rem;
+      }
+
+      div::-webkit-scrollbar-thumb,
+      main::-webkit-scrollbar-thumb,
+      ol::-webkit-scrollbar-thumb,
+      pre::-webkit-scrollbar-thumb,
+      span::-webkit-scrollbar-thumb,
+      ul::-webkit-scrollbar-thumb {
+        background-color: #eff2f5;
+      }
     `,
   ],
 })
