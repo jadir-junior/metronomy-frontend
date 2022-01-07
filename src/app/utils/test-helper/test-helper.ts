11da2b6fe1a136c7f584b6d154e17c1359a17923
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-07 18:23:20 -0300

    disable any on test helper

--- src/app/utils/test-helper/test-helper.ts
+++ src/app/utils/test-helper/test-helper.ts
@@ -2,6 +2,7 @@ import { By } from '@angular/platform-browser'
 import { ComponentFixture } from '@angular/core/testing'
 
 export const getNativeElementByCss = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   fixture: ComponentFixture<any>,
   prop: string
 ): HTMLElement => {
