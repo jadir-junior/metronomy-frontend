6839abb7d763b94047322fe3332d61b7bd7149af
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-07 16:36:06 -0300

    content card scroll

--- src/app/components/card/card-content.component.ts
+++ src/app/components/card/card-content.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-card-content',
-  template: ` <div class="card-content"><ng-content></ng-content></div> `,
+  template: `
+    <div class="card-content" [ngClass]="{ 'card-scroll': scroll }">
+      <ng-content></ng-content>
+    </div>
+  `,
   styles: [
     `
       .card-content {
         padding: 2rem 2.5rem;
       }
+
+      .card-scroll {
+        height: 200px;
+        overflow: auto;
+      }
     `,
   ],
 })
-export class CardContentComponent {}
+export class CardContentComponent {
+  @Input() scroll = false
+}
