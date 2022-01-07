5fe12c3c46abbfd38b1bececa903c417cb6b9522
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-07 14:36:39 -0300

    create component module generic

---
+++ src/app/components.module.ts
@@ -0,0 +1,8 @@
+import { CardModule } from './components/card/card.module'
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  declarations: [],
+  imports: [CardModule],
+})
+export class ComponentsModule {}
