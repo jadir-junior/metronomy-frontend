a0c021f0805f746df9f0cf64ab101f781a934fc9
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-07 18:21:38 -0300

    create a toolbar to header

--- src/app/components/card/card-header.component.ts
+++ src/app/components/card/card-header.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-card-header',
-  template: `<div class="card-header"><ng-content></ng-content></div>`,
+  template: `<div class="card-header">
+    <div class="card-toolbar">
+      <div><ng-content></ng-content></div>
+    </div>
+  </div>`,
   styles: [
     `
       .card-header {
         padding: 2rem 2.5rem;
         border-bottom: 1px solid #eff2f5;
+      }
+
+      .card-toolbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
       }
     `,
   ],
