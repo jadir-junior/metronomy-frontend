80880fee5d8bf10da35aa58a9f36dcfbc1e49e03
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-09 09:30:00 -0300

    add test to link component

--- src/app/components/link/link.component.spec.ts
+++ src/app/components/link/link.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LinkComponent } from './link.component'
+import { getNativeElement } from 'src/app/utils/test-helper/test-helper'
 
 describe('LinkComponent', () => {
   let component: LinkComponent
@@ -15,10 +16,27 @@ describe('LinkComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LinkComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
+    fixture.detectChanges()
     expect(component).toBeTruthy()
+  })
+
+  it('should render link default', () => {
+    fixture.detectChanges()
+
+    const link: HTMLAnchorElement = getNativeElement(fixture, 'a') as HTMLAnchorElement
+
+    expect(getComputedStyle(link).fontWeight).toEqual('500')
+    expect(getComputedStyle(link).fontSize).toBe('17.2px')
+  })
+
+  it('should render a link with as medium size', () => {
+    component.size = 'medium'
+    fixture.detectChanges()
+    const link: HTMLAnchorElement = getNativeElement(fixture, 'a') as HTMLAnchorElement
+
+    expect(getComputedStyle(link).fontSize).toBe('19.2px')
   })
 })
