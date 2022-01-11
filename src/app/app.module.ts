f36e162e4415d05da9c749dcf7a9fcb47b0468dc
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-10 23:16:16 -0300

    add module input to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { CardModule } from './components/card/card.module'
 import { ComponentsModule } from './components.module'
+import { InputModule } from './components/input/input.module'
 import { LinkModule } from './components/link/link.module'
 import { NgModule } from '@angular/core'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule, CardModule, LinkModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    CardModule,
+    LinkModule,
+    InputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
