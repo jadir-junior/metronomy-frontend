df187b2fbebb3d922328ed38dbf49848e03f0ed0
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-20 17:38:57 -0300

    add label

--- src/app/components/input/input.module.ts
+++ src/app/components/input/input.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { InputComponent } from './input.component'
 import { NgModule } from '@angular/core'
+import { LabelComponent } from './label.component'
 
-const components = [InputComponent]
+const components = [InputComponent, LabelComponent]
 
 @NgModule({
   declarations: [...components],
---
+++ src/app/components/input/label.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-label',
+  template: `
+    <p>
+      label works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
