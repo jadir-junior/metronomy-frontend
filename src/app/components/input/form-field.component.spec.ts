8f33566c908dd252c091b6201254f72090564736
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-20 22:54:54 -0300

    test in form field

--- src/app/components/input/form-field.component.spec.ts
+++ src/app/components/input/form-field.component.spec.ts
@@ -1,25 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular'
 
-import { FormFieldComponent } from './form-field.component';
+import { FormFieldComponent } from './form-field.component'
 
 describe('FormFieldComponent', () => {
-  let component: FormFieldComponent;
-  let fixture: ComponentFixture<FormFieldComponent>;
+  it('should create form field appearance default', async () => {
+    await render(
+      '<met-form-field><input placeholder="email@email.com" /></met-form-field>',
+      {
+        declarations: [FormFieldComponent],
+      }
+    )
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FormFieldComponent ]
-    })
-    .compileComponents();
-  });
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toHaveClass(
+      'default'
+    )
+  })
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('should create form field appearance solid', async () => {
+    await render(
+      '<met-form-field appearance="solid"><input placeholder="email@email.com" /></met-form-field>',
+      {
+        declarations: [FormFieldComponent],
+      }
+    )
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toHaveClass('solid')
+  })
+
+  it('should create form field appearance transparent', async () => {
+    await render(
+      '<met-form-field appearance="transparent"><input placeholder="email@email.com" /></met-form-field>',
+      {
+        declarations: [FormFieldComponent],
+      }
+    )
+
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('email@email.com').parentNode).toHaveClass(
+      'transparent'
+    )
+  })
+})
