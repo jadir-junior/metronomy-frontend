ac1ac4a8da2cd015f96b22d528dc5cd30f68e77f
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-01-20 21:58:08 -0300

    create form-field

---
+++ src/app/components/input/form-field.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  template: `
+    <p>
+      form-field works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FormFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/input/input.module.ts
+++ src/app/components/input/input.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { ErrorComponent } from './error.component'
+import { FormFieldComponent } from './form-field.component'
 import { InputComponent } from './input.component'
 import { LabelComponent } from './label.component'
 import { NgModule } from '@angular/core'
 
-const components = [InputComponent, LabelComponent, ErrorComponent]
+const components = [InputComponent, LabelComponent, ErrorComponent, FormFieldComponent]
 
 @NgModule({
   declarations: [...components],
